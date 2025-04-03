4d1843c210ba12dd4af29b1459682d213a00d322
RafeAaron <rafeaaron21@gmail.com>
2025-04-03 22:58:06 +0300

    main java file

---
+++ main.java
@@ -0,0 +1,16 @@
+import MapLibrary.*;
+
+public class main{
+
+    public static void main(String[] args)
+    {
+
+        ShapeFile shpFile = new ShapeFile("MapLibrary/map files/gis_osm_buildings_a_free_1");
+        //shpFile.printFirst150ShapeData();
+
+        //float number = 2/200f;
+
+        //System.out.println("Percentage: " + number);
+    }
+
+}
