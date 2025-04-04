7fe34142c0933586f5a854a8f9485525ac24de31
RafeAaron <rafeaaron21@gmail.com>
2025-04-04 13:23:35 +0300

    Working PolyLine And MultiPoint implementations

--- MapLibrary/MultiPoint.java
+++ MapLibrary/MultiPoint.java
@@ -1,17 +1,78 @@
 package MapLibrary;
 
+import java.nio.ByteBuffer;
+
 public class MultiPoint {
     
     int id;
     int numberOfPoints;
     Point[] points;
 
-    BoundingBox multiLineBoundingBox;
+    BoundingBox multiPointBoundingBox;
 
     public MultiPoint(int[] data)
     {
+
+        byte[] minXArray = new byte[8];
+        byte[] minYArray = new byte[8];
+        byte[] maxXArray = new byte[8];
+        byte[] maxYArray = new byte[8];
         
-        
+        for(int i = 0; i < 32; i++)
+        {
+            if(i < 8)
+            {
+
+                minXArray[i] = (byte)data[i];
+
+            }else if(i < 16 && i >=8)
+            {
+
+                minYArray[i - 8] = (byte)data[i];
+
+            }else if(i < 24 && i >=16)
+            {
+
+                maxXArray[i - 16] = (byte)data[i];
+
+            }
+            else{
+
+                maxYArray[i - 24] = (byte)data[i];
+
+            }
+        }
+
+        double minX = ByteBuffer.wrap(minXArray).getDouble();
+        double minY = ByteBuffer.wrap(minYArray).getDouble();
+        double maxX = ByteBuffer.wrap(maxXArray).getDouble();
+        double maxY = ByteBuffer.wrap(maxYArray).getDouble();
+
+        this.multiPointBoundingBox = new BoundingBox(minX, minY, maxX, maxY);
+
+        int numberOfPoints = data[35];
+        numberOfPoints = numberOfPoints << 8 | data[34];
+        numberOfPoints = numberOfPoints << 8 | data[33];
+        numberOfPoints = numberOfPoints << 8 | data[32];
+
+        this.numberOfPoints = numberOfPoints;
+
+        int byteOffset = 36;
+
+        for(int a = 0; a < this.numberOfPoints;a++)
+                {
+                    int[] arrayOfBytes = new int[16];
+
+                    Point currentPoint;
+
+                    for(int b = 0; b < 16; b++)
+                    {
+                        arrayOfBytes[b] = data[byteOffset + (a * 16) + b];
+                    }
+
+                    currentPoint = new Point(arrayOfBytes);
+                    this.addPoint(currentPoint);
+                }
 
     }
 
@@ -62,7 +123,7 @@ public class MultiPoint {
     public BoundingBox getBoundingBox()
     {
 
-        return this.multiLineBoundingBox;
+        return this.multiPointBoundingBox;
 
     }
 
--- MapLibrary/PolyLine.java
+++ MapLibrary/PolyLine.java
@@ -1,14 +1,158 @@
 package MapLibrary;
 
+import java.nio.ByteBuffer;
+
 public class PolyLine {
 
     private BoundingBox polylineBoundingBox;
-    MultiPoint[] points;
+    int numberOfParts;
+    int numberOfPoints;
+    int[] parts;    
+    MultiPoint[] polyline;
     
     public PolyLine(int[] data)
     {
 
+        byte[] minXArray = new byte[8];
+        byte[] minYArray = new byte[8];
+        byte[] maxXArray = new byte[8];
+        byte[] maxYArray = new byte[8];
         
+        for(int i = 0; i < 32; i++)
+        {
+            if(i < 8)
+            {
+
+                minXArray[i] = (byte)data[i];
+
+            }else if(i < 16 && i >=8)
+            {
+
+                minYArray[i - 8] = (byte)data[i];
+
+            }else if(i < 24 && i >=16)
+            {
+
+                maxXArray[i - 16] = (byte)data[i];
+
+            }
+            else{
+
+                maxYArray[i - 24] = (byte)data[i];
+
+            }
+        }
+
+        double minX = ByteBuffer.wrap(minXArray).getDouble();
+        double minY = ByteBuffer.wrap(minYArray).getDouble();
+        double maxX = ByteBuffer.wrap(maxXArray).getDouble();
+        double maxY = ByteBuffer.wrap(maxYArray).getDouble();
+
+        this.polylineBoundingBox = new BoundingBox(minX, minY, maxX, maxY);
+
+        int numberOfPartsInPolyLine = data[35];
+        numberOfPartsInPolyLine = numberOfPartsInPolyLine << 8 | data[34];
+        numberOfPartsInPolyLine = numberOfPartsInPolyLine << 8 | data[33];
+        numberOfPartsInPolyLine = numberOfPartsInPolyLine << 8 | data[32];
+
+        this.numberOfParts = numberOfPartsInPolyLine;
+
+        int numberOfPointsInPolyLine = data[39];
+        numberOfPointsInPolyLine = numberOfPointsInPolyLine << 8 | data[38];
+        numberOfPointsInPolyLine = numberOfPointsInPolyLine << 8 | data[37];
+        numberOfPointsInPolyLine = numberOfPointsInPolyLine << 8 | data[36];
+
+        this.numberOfPoints = numberOfPointsInPolyLine;
+
+        this.parts = new int[this.numberOfParts];
+
+        for(int i = 0; i < numberOfPartsInPolyLine; i++)
+        {
+            int a = data[40 + (4 * i) + 3];
+            a = a << 8 | data[40 + (4 * i) + 2];
+            a = a << 8 | data[40 + (4 * i) + 1];
+            a = a << 8 | data[40 + (4 * i) + 0];
+
+            this.parts[i] = a;
+
+        }
+
+        this.polyline = new MultiPoint[this.numberOfParts];
+
+        int byteOffset = 40 + (4 * this.numberOfParts);
+
+        for(int i = 0; i < numberOfPartsInPolyLine; i++)
+        {
+            MultiPoint pointMulti = new MultiPoint();
+            this.polyline[i] = pointMulti;
+
+            if(numberOfPartsInPolyLine == 1){
+
+                for(int a = 0; a < this.numberOfPoints;a++)
+                {
+                    int[] arrayOfBytes = new int[16];
+
+                    Point currentPoint;
+
+                    for(int b = 0; b < 16; b++)
+                    {
+                        arrayOfBytes[b] = data[byteOffset + (a * 16) + b];
+                    }
+
+                    currentPoint = new Point(arrayOfBytes);
+                    pointMulti.addPoint(currentPoint);
+                }
+
+            }else{
+
+                if(i == numberOfPartsInPolyLine - 1)
+                {
+
+                    int numberOfPointsRemaining = this.numberOfPoints - this.parts[i];
+                    int startingpoint = this.parts[i];
+
+                    for(int c = 0; c < numberOfPointsRemaining; c++)
+                    {
+                        int[] arrayOfBytes = new int[16];
+                        Point currentPoint;
+
+                        for(int d = 0; d < 16; d++)
+                    {
+                        arrayOfBytes[d] = data[byteOffset + (startingpoint * 16) + d];
+                    }
+
+                    currentPoint = new Point(arrayOfBytes);
+                    pointMulti.addPoint(currentPoint);
+                    startingpoint++;
+
+                    }
+
+                }else{
+
+                    int numberOfPointsRemaining = this.parts[i + 1] - this.parts[i];
+                    int startingpoint = this.parts[i];
+
+                    for(int c = 0; c < numberOfPointsRemaining; c++)
+                    {
+
+                        int[] arrayOfBytes = new int[16];
+                        Point currentPoint;
+
+                        for(int d = 0; d < 16; d++)
+                        {
+                            arrayOfBytes[d] = data[byteOffset + (startingpoint * 16) + d];
+                        }
+
+                    currentPoint = new Point(arrayOfBytes);
+                    pointMulti.addPoint(currentPoint);
+                    startingpoint++;
+
+                    }
+
+                }
+
+            }
+        }
 
     }
 
@@ -21,7 +165,7 @@ public class PolyLine {
 
     public MultiPoint[] getlines()
     {
-        return this.points;
+        return this.polyline;
     }
 
 }
